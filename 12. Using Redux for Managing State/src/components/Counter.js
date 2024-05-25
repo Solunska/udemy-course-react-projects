a7daaa63e60c443dee544c8c33a4e1d5f025568f
solunska <a_solunska@outlook.com>
2024-05-25 12:51:17 +0200

    react redux basics

---
+++ 12. Using Redux for Managing State/src/components/Counter.js
@@ -0,0 +1,39 @@
+import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { INCREMENT } from '../store/index';
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
+
+  function incrementHandler() {
+    dispatch({ type: INCREMENT });
+  }
+  function increaseHandler() {
+    dispatch({ type: 'increase', value: 5 });
+  }
+
+  function decrementHandler() {
+    dispatch({ type: 'decrement' });
+  }
+
+  function toggleCounterHandler() {
+    dispatch({ type: 'toggle' });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
