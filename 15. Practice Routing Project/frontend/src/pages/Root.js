5eca06c5ff7de94c5059a3b1a886e585f89cd0bf
solunska <a_solunska@outlook.com>
2024-06-05 14:24:56 +0200

    practice project

---
+++ 15. Practice Routing Project/frontend/src/pages/Root.js
@@ -0,0 +1,9 @@
+import MainNavigation from '../components/MainNavigation';
+import { Outlet } from 'react-router-dom';
+
+export default function RootLayout() {
+    return <>
+        <MainNavigation />
+        <Outlet />
+    </>
+}
